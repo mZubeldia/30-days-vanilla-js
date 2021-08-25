2d410a4dd8096544c2a0cd1b8a12680c3028bc83
mZubeldia <mzubeldia09@gmail.com>
2021-08-25 21:07:56 +0200

    add minute and second functionality

--- 02/01.js
+++ 02/01.js
@@ -1,3 +1,22 @@
-"use strickt";
+"use strict";
 
 console.log("hola");
+
+const secondHand = document.querySelector(".second-hand");
+const minuteHand = document.querySelector(".min-hand");
+
+function setDate() {
+  //gets the actual date and updates de second hand
+
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const minutes = now.getMinutes();
+
+  const secondsDegrees = (seconds / 60) * 360 + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+  const minutesDegrees = (minutes / 60) * 360 + 90;
+  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
