97aa029af32c02347981a160299d64349ce8b7ec
mZubeldia <mzubeldia09@gmail.com>
2021-08-25 21:15:15 +0200

    add hour functionality

--- 02/01.js
+++ 02/01.js
@@ -4,6 +4,7 @@ console.log("hola");
 
 const secondHand = document.querySelector(".second-hand");
 const minuteHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
 
 function setDate() {
   //gets the actual date and updates de second hand
@@ -11,12 +12,16 @@ function setDate() {
   const now = new Date();
   const seconds = now.getSeconds();
   const minutes = now.getMinutes();
+  const hour = now.getHours();
 
   const secondsDegrees = (seconds / 60) * 360 + 90;
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
   const minutesDegrees = (minutes / 60) * 360 + 90;
   minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+  const hourDegrees = (hour / 12) * 360 + 90;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
 
 setInterval(setDate, 1000);
